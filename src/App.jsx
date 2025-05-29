5108ff62aed372378604547e48e55587a0bbd3c6
jngmnj <exorbitance@naver.com>
2025-05-29 17:28:34 +0900

    feat: 메인페이지 컨텐츠 변경(CICD 테스트)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,17 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Github Actions를 통한 CI/CD</h1>
+      <p>
+        Vite + React + Github Actions를 이용한 CI/CD 예제입니다.
       </p>
     </>
   )
